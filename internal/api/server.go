2d54d9bdb31156a022d47dab8ac08069a157b91c
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-23 13:11:13 +0100

    add logic to fetch another user's achievement

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,5 +1,19 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
-func SonyServer(w http.ResponseWriter, r *http.Request) {}
+func SonyServer(w http.ResponseWriter, r *http.Request) {
+	userId := strings.Split(r.URL.String(), "/")[2]
+
+	if userId == "1" {
+		fmt.Fprintf(w, "Bronze")
+	}
+
+	if userId == "2" {
+		fmt.Fprintf(w, "Silver")
+	}
+}
