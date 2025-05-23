bc48f3d148c7295940988d9df8cdd9503c409b66
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-23 12:53:06 +0100

    add test case stub to fetch user achievement

---
+++ internal/test/user_acheivement_level_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/sonylevelup/internal/api"
+)
+
+func TestGetUserAcheivementLevel(t *testing.T) {
+	t.Run("return garrys achievement level", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/user/1/acheivement-level", nil)
+		response := httptest.NewRecorder()
+
+		api.SonyServer(response, request)
+
+		got := response.Body.String()
+		want := "Bronze"
+
+		if got != want {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
+	})
+}
