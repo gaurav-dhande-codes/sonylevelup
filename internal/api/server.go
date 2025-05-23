c26d73e946380ea5b32b1196df52b95b317582ea
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-23 20:46:48 +0100

    update user store interface to include new functions for fetching user, game library and user game achievement completion

--- internal/api/server.go
+++ internal/api/server.go
@@ -3,11 +3,18 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
+type User struct {
+	Id int
+}
+
 type UserStore interface {
-	GetUserAchievementLevel(userId string) string
+	GetUser(userId int) int
+	GetUserGameLibrary(userId int) []int
+	GetUserGameAchievementCompletion(userId, gameId int) map[string]int
 }
 
 type SonyServer struct {
@@ -20,15 +27,33 @@ func NewSonyServer(store UserStore) *SonyServer {
 
 func (s *SonyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	userId := strings.Split(r.URL.String(), "/")[2]
-	fmt.Fprintf(w, "%s", s.store.GetUserAchievementLevel(userId))
+	intUserId, _ := strconv.Atoi(userId)
+
+	_ = s.store.GetUser(intUserId)
+	_ = s.store.GetUserGameLibrary(intUserId)
+	_ = s.store.GetUserGameAchievementCompletion(intUserId, 0)
+
+	fmt.Fprintf(w, "%s", GetUserAchievementLevel(intUserId))
 }
 
-func GetUserAchievementLevel(userId string) string {
-	if userId == "1" {
+func GetUser(userId int) int {
+	return 0
+}
+
+func GetUserGameLibrary(userId int) []int {
+	return []int{}
+}
+
+func GetUserGameAchievementCompletion(userId, gameId int) map[string]int {
+	return map[string]int{}
+}
+
+func GetUserAchievementLevel(userId int) string {
+	if userId == 1 {
 		return "Bronze"
 	}
 
-	if userId == "2" {
+	if userId == 2 {
 		return "Silver"
 	}
 
