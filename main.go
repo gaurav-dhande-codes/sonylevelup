e24c8a2f5eca46f7e17c663cc7fb3ccc904dd8e5
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-23 13:12:50 +0100

    add server handler to listen for requests

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/sonylevelup/internal/api"
+)
 
 func main() {
 	fmt.Println("Welcome to Sony Level Up!")
+	handler := http.HandlerFunc(api.SonyServer)
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
