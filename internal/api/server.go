24ba9dd2edaf6731021aba43239908d88f7c19af
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-25 19:10:34 +0100

    remove the api logic to fetch user achievement level and add routing using gorilla mux

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,12 +1,9 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
-	"github.com/sonylevelup/internal/pkg"
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -57,59 +54,10 @@ func GetGameAchievementCompletionPercentage(completedAchievement, totalAchieveme
 }
 
 func (s *SonyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	userId := strings.Split(r.URL.String(), "/")[2]
-	intUserId, _ := strconv.Atoi(userId)
+	router := mux.NewRouter()
+	router.HandleFunc("/user/{userId}/achievement-level", s.GetUserAchievementLevel)
 
-	userGameLibrary := s.store.GetUserGameLibrary(intUserId)
-
-	// Check if the user owns 10 or fewer games
-	if len(userGameLibrary.OwnedGames) <= 10 {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, pkg.NoAchievementLevel)
-		return
-	}
-
-	PlatinumAchievement := AchievementLevel{pkg.PlatinumAchievementLevel, 50, 99.99}
-	GoldAchievement := AchievementLevel{pkg.GoldAchievementLevel, 25, 80.00}
-	SilverAchievement := AchievementLevel{pkg.SilverAchievementLevel, 10, 75.00}
-	BronzeAchievement := AchievementLevel{pkg.BronzeAchievementLevel, 10, 00.00}
-
-	possibleAchievementLevels := []AchievementLevel{
-		PlatinumAchievement,
-		GoldAchievement,
-		SilverAchievement,
-		BronzeAchievement,
-	}
-
-	currentIndex := 0
-
-	for _, game := range userGameLibrary.OwnedGames {
-		if possibleAchievementLevels[currentIndex].Name == pkg.BronzeAchievementLevel {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintf(w, pkg.BronzeAchievementLevel)
-
-			return
-		}
-
-		gameAchievementCompletion := s.store.GetUserGameAchievementCompletion(intUserId, game.Id)
-
-		// Calculate the completion percentage for the game
-		gameAchievementCompletionPercentage := GetGameAchievementCompletionPercentage(
-			gameAchievementCompletion.TotalCompletedAchievements,
-			gameAchievementCompletion.Game.TotalAvailableAchievements)
-
-		for currentIndex+1 < len(possibleAchievementLevels) {
-			if gameAchievementCompletionPercentage > possibleAchievementLevels[currentIndex].AchievementThreshold &&
-				len(userGameLibrary.OwnedGames) > possibleAchievementLevels[currentIndex].OwnedGamesThreshold {
-				break
-			} else {
-				currentIndex++
-			}
-		}
-	}
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s", possibleAchievementLevels[currentIndex].Name)
+	router.ServeHTTP(w, r)
 }
 
 func GetUser(userId int) *User {
