98d9471f4e7d54f04031b8cc0629baa2f46bc683
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-26 12:10:30 +0100

    add json struct tags for user library models

--- internal/model/user_library.go
+++ internal/model/user_library.go
@@ -1,18 +1,18 @@
 package model
 
 type UserLibrary struct {
-	User       User
-	OwnedGames []Game
+	User       User   `json:"user"`
+	OwnedGames []Game `json:"ownedGames"`
 }
 
 type Game struct {
-	Id                         int
-	Title                      string
-	TotalAvailableAchievements int
+	Id                         int    `json:"id"`
+	Title                      string `json:"title"`
+	TotalAvailableAchievements int    `json:"totalAvailableAchievements"`
 }
 
 type UserGameAchievementCompletion struct {
-	User                       User
-	Game                       Game
-	TotalCompletedAchievements int
+	User                       User `json:"user"`
+	Game                       Game `json:"game"`
+	TotalCompletedAchievements int  `json:"totalCompletedAchievements"`
 }
