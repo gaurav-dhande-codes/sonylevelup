261ce00ae9ebc49bdf22e89818d067fc6912af9e
gauravdhande <gaurav.b.dhande@gmail.com>
2025-05-26 16:50:27 +0100

    add utility functions for returning standard error and valid response body for api controllers

--- internal/api/utils.go
+++ internal/api/utils.go
@@ -1,5 +1,16 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 func GetGameAchievementCompletionPercentage(completedAchievement, totalAchievements int) float64 {
 	return float64(completedAchievement) / float64(totalAchievements) * 100
+}
+
+func WriteValidResponseToResponseWriter(w http.ResponseWriter, statusCode int, responseBody any) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(responseBody)
 }
--- internal/pkg/errors.go
+++ internal/pkg/errors.go
@@ -1,8 +1,48 @@
 package pkg
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	Error   string `json:"error"`
+	Message string `json:"message"`
+}
 
 var (
-	ErrUserNotFound       = errors.New("user not found")
-	ErrUserOrGameNotFound = errors.New("user or game not found")
+	ErrUserNotFound        = errors.New("user not found")
+	ErrUserOrGameNotFound  = errors.New("user or game not found")
+	ErrInvalidUserID       = errors.New("user id provided is invalid")
+	ErrInternalServerError = errors.New("an unexpected error occurred. Please try again later")
 )
+
+const (
+	BadRequest          = "BAD_REQUEST"
+	UserNotFound        = "USER_NOT_FOUND"
+	UserOrGameNotFound  = "USER_OR_GAME_NOT_FOUND"
+	InternalServerError = "INTERNAL_SERVER_ERROR"
+)
+
+var (
+	ErrorMapping = map[error]string{
+		ErrUserNotFound:        UserNotFound,
+		ErrUserOrGameNotFound:  UserOrGameNotFound,
+		ErrInternalServerError: InternalServerError,
+		ErrInvalidUserID:       BadRequest,
+	}
+)
+
+func WriteErrorResponseToResponseWriter(w http.ResponseWriter, statusCode int, err error) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(GetErrorResponseBody(ErrorMapping[err], err.Error()))
+}
+
+func GetErrorResponseBody(errorCtx, message string) *ErrorResponse {
+	return &ErrorResponse{
+		Error:   errorCtx,
+		Message: message,
+	}
+}
